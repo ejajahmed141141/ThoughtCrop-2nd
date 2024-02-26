b4f215ddf35dabe1289d25baacbae1a29896d8ae
Ejaj <ejajahmed141141@gmail.com>
2024-02-26 10:43:46 +0530

    first commit

---
+++ th.js
@@ -0,0 +1,184 @@
+// 1st container...
+
+let hover = document.querySelector('#hover')
+        let text = document.querySelector('#text') 
+        hover.addEventListener("mouseover" , () => {
+            text.style.display = "block"
+            hover.style.backgroundColor = "Darkgray";
+        })
+        hover.addEventListener("mouseout" , () => {
+            text.style.display = "none"
+            hover.style.backgroundColor = "transparent";
+        })
+
+
+        // 2nd container j.s
+        let hover2 = document.querySelector('#hover2')
+        let text2 = document.querySelector('#text2')
+        hover2.addEventListener("mouseover" , () => {
+            text2.style.display = "block"
+            hover2.style.backgroundColor = "Darkgray";
+        })
+        hover2.addEventListener("mouseout" , () => {
+            text2.style.display = "none"
+            hover2.style.backgroundColor = "transparent";
+        })
+
+        // 3rd container....
+
+        let hover3 = document.querySelector('#hover3')
+        let text3 = document.querySelector('#text3')
+        hover3.addEventListener("mouseover" , () => {
+            text3.style.display = "block"
+            hover3.style.backgroundColor = "Darkgray";
+        })
+        hover3.addEventListener("mouseout" , () => {
+            text3.style.display = "none"
+            hover3.style.backgroundColor = "transparent";
+        })
+
+        // 4th container
+        
+        let hover4 = document.querySelector('#hover4')
+        let text4 = document.querySelector('#text4')
+        hover4.addEventListener("mouseover" , () => {
+            text4.style.display = "block"
+            hover4.style.backgroundColor = "Darkgray";
+        })
+        hover4.addEventListener("mouseout" , () => {
+            text4.style.display = "none"
+            hover4.style.backgroundColor = "transparent";
+        })
+
+        // 5th container
+        
+        let hover5 = document.querySelector('#hover5')
+        let text5 = document.querySelector('#text5')
+        hover5.addEventListener("mouseover" , () => {
+            text5.style.display = "block"
+            // hover5.style.backgroundColor = "Darkgray";
+        })
+        hover5.addEventListener("mouseout" , () => {
+            text5.style.display = "none"
+            // hover5.style.backgroundColor = "Darkgray";
+        })
+
+        // container6
+        
+        let hover6 = document.querySelector('#hover6')
+        let text6 = document.querySelector('#text6')
+        hover6.addEventListener("mouseover" , () => {
+            text6.style.display = "block"
+        })
+        hover6.addEventListener("mouseout" , () => {
+            text6.style.display = "none"
+            // hover6.style.backgroundColor = "Darkgray";
+        })
+
+        // 7th Container
+        
+        let hover7 = document.querySelector('#hover7')
+        let text7 = document.querySelector('#text7')
+        hover7.addEventListener("mouseover" , () => {
+            text7.style.display = "block"
+        })
+        hover7.addEventListener("mouseout" , () => {
+            text7.style.display = "none"
+            // hover7.style.backgroundColor = "Darkgray";
+        })
+
+    
+        let hover8  = document.querySelector('#hover8')
+        let text8 = document.querySelector('#text8')
+        hover8.addEventListener("mouseover" , () => {
+            text8.style.display = "block"
+        })
+        hover8.addEventListener("mouseout" , () => {
+            text8.style.display = "none"
+            // hover8.style.backgroundColor = "Darkgray";
+        })
+
+
+        
+        let hover9 = document.querySelector('#hover9')
+        let text9 = document.querySelector('#text9')
+        hover9.addEventListener("mouseover" , () => {
+            text9.style.display = "block"
+        })
+        hover9.addEventListener("mouseout" , () => {
+            text9.style.display = "none"
+        })
+
+
+
+        
+        let hover10 = document.querySelector('#hover10')
+        let text10 = document.querySelector('#text10')
+        hover10.addEventListener("mouseover" , () => {
+            text10.style.display = "block"
+        })
+        hover10.addEventListener("mouseout" , () => {
+            text10.style.display = "none"
+        })
+
+
+        
+        let hover11 = document.querySelector('#hover11')
+        let text11 = document.querySelector('#text11')
+        hover11.addEventListener("mouseover" , () => {
+            text11.style.display = "block"
+        })
+        hover11.addEventListener("mouseout" , () => {
+            text11.style.display = "none"
+        })
+
+
+        let hover12 = document.querySelector('#hover12')
+        let text12 = document.querySelector('#text12')
+        hover12.addEventListener("mouseover" , () => {
+            text12.style.display = "block"
+        })
+        hover12.addEventListener("mouseout" , () => {
+            text12.style.display = "none"
+        })
+
+        let hover13 = document.querySelector('#hover13')
+        let text13= document.querySelector('#text13')
+        hover13.addEventListener("mouseover" , () => {
+            text13.style.display = "block"
+        })
+        hover13.addEventListener("mouseout" , () => {
+            text13.style.display = "none"
+        })
+        
+        let hover14= document.querySelector('#hover14')
+        let text14 = document.querySelector('#text14')
+        hover14.addEventListener("mouseover" , () => {
+            text14.style.display = "block"
+        })
+        hover14.addEventListener("mouseout" , () => {
+            text14.style.display = "none"
+        })
+
+
+
+        
+        let hover15 = document.querySelector('#hover15')
+        let text15 = document.querySelector('#text15')
+        hover15.addEventListener("mouseover" , () => {
+            text15.style.display = "block"
+        })
+        hover15.addEventListener("mouseout" , () => {
+            text15.style.display = "none"
+        })
+
+
+        
+        let hover16 = document.querySelector('#hover16')
+        let text16 = document.querySelector('#text16')
+        hover16.addEventListener("mouseover" , () => {
+            text16.style.display = "block"
+        })
+        hover16.addEventListener("mouseout" , () => {
+            text16.style.display = "none"
+        })
